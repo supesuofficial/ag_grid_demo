2d10fc6aa93911d638cd8ce54b51eeecec982dcf
Don Foh Cher Ern <don_foh@hotmail.com>
2022-04-12 12:33:30 +0800

    updates to make compatible with current backend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useState, useEffect} from "react"
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+const Papa = require("papaparse")
+
 
 const App = () => {
 
@@ -16,26 +18,21 @@ const App = () => {
     setColumnApi(params.columnApi)
     params.columnApi.autoSizeAllColumns()
   }
-  const [rowData, setRowData] = useState([
+  const [rowData] = useState([
     
   ])
 
-  const [colDefs, setColDefs] = useState([
+  const [colDefs] = useState([
     //{headerName: "", checkboxSelection: true, headerCheckboxSelection: true, pinned: "left"},
-    {field: 'measurement', headerName: "Measurement", editable: true, lockPosition: true, resizable: true},
-    {field: 'easeTight', headerName: "Ease Tight", editable: true, lockPosition: true, resizable: true},
-    {field: 'easeLoose', headerName: "Ease Loose", editable: true, lockPosition: true, resizable: true},
-    {field: 'bodyPart', editable: true, lockPosition: true, resizable: true},
+    {field: 'dimension', headerName: "Dimension", editable: true, lockPosition: true, resizable: true},
     {field: 'xs', editable: true, resizable: true},
     {field: 's', editable: true, resizable: true},
     {field: 'm', editable: true, resizable: true},
-    {field: 'l', editable: true, resizable: true}
+    {field: 'l', editable: true, resizable: true},
+    {field: 'xl', editable: true, resizable: true},
+    {field: 'xxl', editable: true, resizable: true},
   ]);
 
-
-  const addColumn = () => {
-    setColDefs(colDefs.concat({headerName: newColName ? newColName : "Unnamed size", editable: true, resizable: true, field: `${Date.now()}`}))
-  }
 
   const addRow = () => {
     gridApi.applyTransaction({ add: [{}]})
@@ -47,26 +44,69 @@ const App = () => {
   }
 
   const reAddRow = (e) => {
-    gridApi.applyTransaction( { add: [e.node.data]})
+    // Check that row doesn't already exist
+    if (gridApi.getRowNode(e.node.id) == undefined){
+      gridApi.applyTransaction( { add: [e.node.data]})
+    }
   }
 
 
   const showData = () => {
-    const columns = columnApi.getAllDisplayedColumns()
-    const csv = gridApi.getDataAsCsv({columnKeys: columns.filter(column => column.colId !== "0")})
-    console.log(csv)
+   let data = []
+   gridApi.forEachNode(row => data.push(row.data))
+
+   const parsedData = data.map(row => {
+    const {dimension, ...sizes} = row
+
+    let newRow = {}
+    newRow.dimension = row.dimension
+    newRow.sizes = sizes
+
+    return newRow
+   })
+
+   console.log(parsedData)
   }
 
-  const handleNameChange = (e) => {
-    setNewColName(e.target.value)
+  const handleImport = (e) => {
+    console.log("Imported")
+
+    const csv = e.target.files[0]
+
+    const setGridRows = (results, file) => {
+      // results will have error also, can do error handling
+      gridApi.setRowData(results.data)
+    }
+
+    Papa.parse(csv, 
+      {
+        complete: setGridRows,
+        skipEmptyLines: true,
+        header: true,
+        transformHeader: (header) => header.toLowerCase()
+      }
+    )
+  }
+
+  const receiveData = () => {
+    const data = [{dimension: "test", sizes: {s:25, xxs:10}}]
+    const parsedData = data.map(row => {
+      let newRow = {...row.sizes}
+      newRow.dimension = row.dimension
+
+      return newRow
+    })
+
+    console.log(parsedData)
+    gridApi.setRowData(parsedData)
   }
 
   return (
     <>
       <button onClick={addRow}> Add row</button>
       <button onClick={showData}> Show state </button>
-      <input type="text" placeholder='Column name' onChange={handleNameChange} required={true}/>
-      <button onClick={addColumn}> Add column </button>
+      <button onClick={receiveData}> Receive data</button>
+      <input type="file" accept=".csv" onChange={handleImport}/>
       <div className="ag-theme-alpine" style={{height: 400, width: 800}}>
         <AgGridReact 
           rowData={rowData}
